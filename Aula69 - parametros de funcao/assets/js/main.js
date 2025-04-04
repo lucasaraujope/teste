29a7549c0ad994214fb34ec26493de3a80044a1d
Lucas Araujo <93890096+lucasaraujope@users.noreply.github.com>
2025-04-04 10:25:40 -0300

    Add files via upload

--- Aula69 - parametros de funcao/assets/js/main.js
+++ Aula69 - parametros de funcao/assets/js/main.js
@@ -5,3 +5,32 @@ function funcao (){
 }
 
 funcao('valor', 1, 2, 3, 4, 5)
+
+//quando eu tenho mais parametros como abaixo que vai de "a" até "f" que são mais do que os argumentos que são só 3, e peço um retorno com o mesmo tanto de parametros, parãmetros que estão sem argumentos retornam como undefines
+function numeros (a, b, c, d, e, f){
+    console.log(a, b, c, d, e, f)
+}
+
+numeros(3, 4, 5)
+
+//passando o valor padrão para o parâmetro caso não seja passado nenhum argumento para assumir o parâmetro no caso abaixo eu passei um valor para o b, e como não foi enviado um argumento quando eu chamei a função a função usou o valor de 3 que foi setado no b
+function soma(a, b = 3){
+    console.log(a + b)
+}
+
+soma(5, 12)
+
+
+//atribuição via desestruturação
+function destru({nome, sobrenome, idade}){
+    console.log(nome, sobrenome, idade)
+}
+
+destru({nome: 'Lucas', sobrenome: 'Araujo', idade: 29})
+
+
+function operadorRest(operador, incrementador, ...rest){
+    console.log(operador, incrementador, rest)
+}
+
+operadorRest('+', '++', 1, 2, 3, 4, 5)
